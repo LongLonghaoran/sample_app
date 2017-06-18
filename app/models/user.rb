a3fa5b1c57c6382e031588f9a84d1fbe8211c260
LongLonghaoran <m18687430124@163.com>
2017-06-18 19:24:59 +0800

    创建User模型并添加bcrypt的has_secure_password实现用户密码校验"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  #利用回调函数在对象存进数据库之前转为小写
+  before_save {self.email = self.email.downcase}
   #presence验证存在性，length: {maximum: }验证最大长度
   validates :name,  presence: true, length: {maximum: 50}
   #format: {with: regex}验证数据格式,unique唯一性校验
   validates :email,  presence: true,
   format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},uniqueness: true
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
