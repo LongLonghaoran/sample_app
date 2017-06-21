06b50be18917d02fdeb8af5f89b3554a8981dc41
LongLonghaoran <m18687430124@163.com>
2017-06-22 00:10:00 +0800

    同上

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      #使用flash才能确保登录成功后（先登录失败一次），错误提示消失，now表示flash值在
+      # 当前这次请求中有效,因为这里登录错误之后是render重新渲染页面算不上新的请求
+      # flash仍然会存留到下一次请求的页面中
+      flash.now[:error] = "用户名或密码错误"
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+end
